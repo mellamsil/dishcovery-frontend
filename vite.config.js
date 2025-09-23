ac5401b472fbddb4e6ed1545143675a2725dd9e3
mellamsil <melvinsil@gmail.com>
2025-09-22 23:55:06 -0400

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/dishcovery-frontend/",
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      "/api": "http://localhost:5000", // forward API calls
+      "/api": "http://localhost:5000",
     },
   },
 });
