bf3fa688f92f573881e0fd2988450cbed067d7d2
mellamsil <melvinsil@gmail.com>
2025-11-27 21:16:35 -0500

    Update ESLint config to fix linting setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from "@eslint/js";
 import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import prettier from "eslint-plugin-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
 
@@ -10,15 +9,13 @@ export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{js,jsx}"],
-    plugins: ["react", "react-hooks", "prettier"],
-    extends: [
-      js.configs.recommended,
-      react.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
-    ],
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      prettier,
+    },
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: "latest",
@@ -27,6 +24,15 @@ export default defineConfig([
       },
     },
     rules: {
+      // JS recommended rules
+      ...js.configs.recommended.rules,
+      // React rules
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      // React hooks rules
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      // Custom rules
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
       "react/prop-types": 0,
       "prettier/prettier": ["error", { endOfLine: "lf" }],
